8ebc336675d2a05e3a3d02ece85aeeba8855a8b1
LalithRanjan <lalithranjankumaravel@gmail.com>
2023-07-11 00:13:42 +0530

    changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,28 @@
 import cors from 'cors'
-import express, { Request, Response } from 'express'
-import { sampleProducts } from './data'
+import dotenv from 'dotenv'
+import express from 'express'
+import mongoose from 'mongoose'
+import { productRouter } from './router/productRouter'
+import { seedRouter } from './router/seedRouter'
 
+dotenv.config()
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/amazondemouser'
+mongoose.set('strictQuery', true)
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to mongodb')
+  })
+  .catch(() => {
+    console.log('error mongodb')
+  })
 const app = express()
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:5174'],
+    origin: ['http://localhost:5176'],
   })
 )
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,41 @@
-import { Outlet } from 'react-router-dom'
-import { sampleProducts } from './data'
-import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
+import { Link, Outlet } from 'react-router-dom'
+import { Badge, Button, Container, Nav, Navbar } from 'react-bootstrap'
+import { useContext, useEffect } from 'react'
+import { Store } from './Store'
 
 function App() {
+  const {
+    state: { mode, cart },
+    dispatch,
+  } = useContext(Store)
+
+  useEffect(() => {
+    document.body.setAttribute('data-bs-theme', mode)
+  }, [mode])
+
+  const switchModeHandler = () => {
+    dispatch({ type: 'SWITCH_MODE' })
+  }
+
   return (
     <div className="d-flex flex-column vh-100">
       <header>
-        <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar expand="lg">
           <Container>
             <Navbar.Brand>AMAZON</Navbar.Brand>
           </Container>
           <Nav>
-            <a href="/cart" className="nav-link">
+            <Button variant={mode} onClick={switchModeHandler}>
+              <i className={mode === 'light' ? 'fa fa-sun' : 'fa fa-moon'}></i>
+            </Button>
+            <Link to="/cart" className="nav-link">
               Cart
-            </a>
+              {cart.cartItems.length > 0 && (
+                <Badge pill bg="danger">
+                  {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                </Badge>
+              )}
+            </Link>
             <a href="/signin" className="nav-link">
               Sign In
             </a>
